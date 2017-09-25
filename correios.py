b312722299b71cc7e3f51f1f9c2ab39fa616c229
Bruno <anjozero@gmail.com>
2017-09-24 23:34:29 -0300

    Update correios.py

--- correios.py
+++ correios.py
@@ -1,9 +1,19 @@
+import requests
+import json
+
+
 class rcep():
     def __init__(self, cep):
         self.cep = str(cep)
         self.url = 'https://viacep.com.br/ws/{}/json/'.format(cep)
         re = self.requisicao()
         self.logradouro = re['logradouro']
+        self.cep = re['cep']
+        self.complemento = re['complemento']
+        self.bairro = re['bairro']
+        self.localidade = re['localidade']
+        self.uf = re['uf']
+        self.unidade = re['unidade']
     def getUrl(self):
         return self.url
 
@@ -15,3 +25,4 @@ class rcep():
         except Exception as e:
             print('Erro: ', e)
             exit()
+
